b4b4fcc182521c42f3bfbf3d0fe78b2b38f390c6
Lucas <128428285+LucasCAlbas@users.noreply.github.com>
2024-10-25 23:29:52 -0300

    Update App.js

--- frontend/public/src/App.js
+++ frontend/public/src/App.js
@@ -1,24 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import ClienteList from './components/ClienteList';
 
 function App() {
-  const [clientes, setClientes] = useState([]);
-
-  useEffect(() => {
-    fetch('http://localhost:8080/clientes')
-      .then(response => response.json())
-      .then(data => setClientes(data));
-  }, []);
-
-  return (
-    <div>
-      <h1>Lista de Clientes</h1>
-      <ul>
-        {clientes.map(cliente => (
-          <li key={cliente.id}>{cliente.nome}</li>
-        ))}
-      </ul>
-    </div>
-  );
+    return (
+        <div className="App">
+            <ClienteList />
+        </div>
+    );
 }
 
 export default App;
