72e5e960d772eb79907463f8f4a364ba49f12115
Lucas <128428285+LucasCAlbas@users.noreply.github.com>
2024-10-25 23:21:56 -0300

    Update Venda.java

--- backend/src/main/java/com/kxstore/model/Venda.java
+++ backend/src/main/java/com/kxstore/model/Venda.java
@@ -1,13 +1,20 @@
+package com.kxstore.model;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
 @Entity
 public class Venda {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Date data;
-    private Double valor;
 
     @ManyToOne
     @JoinColumn(name = "cliente_id")
     private Cliente cliente;
+
+    private LocalDate dataVenda;
+    private Double valorTotal;
+
     // Getters e Setters
 }
