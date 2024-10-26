d571caabdf3aeab14efdf20bab3d1e8392d217c6
Lucas <128428285+LucasCAlbas@users.noreply.github.com>
2024-10-25 23:09:34 -0300

    Update Cliente.java

--- backend/src/main/java/com/kxstore/model/Cliente.java
+++ backend/src/main/java/com/kxstore/model/Cliente.java
@@ -8,17 +8,3 @@ public class Cliente {
     private String telefone;
     // Getters e Setters
 }
-
-@Entity
-public class Venda {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private Date data;
-    private Double valor;
-
-    @ManyToOne
-    @JoinColumn(name = "cliente_id")
-    private Cliente cliente;
-    // Getters e Setters
-}
