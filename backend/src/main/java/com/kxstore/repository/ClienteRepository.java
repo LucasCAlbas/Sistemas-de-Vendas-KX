907f7275e557d67ba2b8aa35f43a33630c3a4416
Lucas <128428285+LucasCAlbas@users.noreply.github.com>
2024-10-25 23:23:57 -0300

    Update ClienteRepository.java

--- backend/src/main/java/com/kxstore/repository/ClienteRepository.java
+++ backend/src/main/java/com/kxstore/repository/ClienteRepository.java
@@ -1,4 +1,7 @@
-@Repository
+package com.kxstore.repository;
+
+import com.kxstore.model.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 }
-
