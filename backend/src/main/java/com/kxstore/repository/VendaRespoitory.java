7d7665c9346975dc3be69aaa9a8369a2f2de6cf3
Lucas <128428285+LucasCAlbas@users.noreply.github.com>
2024-10-25 23:22:16 -0300

    Update VendaRespoitory.java

--- backend/src/main/java/com/kxstore/repository/VendaRespoitory.java
+++ backend/src/main/java/com/kxstore/repository/VendaRespoitory.java
@@ -1,3 +1,7 @@
-@Repository
+package com.kxstore.repository;
+
+import com.kxstore.model.Venda;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 public interface VendaRepository extends JpaRepository<Venda, Long> {
 }
