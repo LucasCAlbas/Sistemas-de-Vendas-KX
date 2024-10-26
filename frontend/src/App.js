842ac05a5ff37bf9e0406d0aae67999edb402c4c
Lucas <128428285+LucasCAlbas@users.noreply.github.com>
2024-10-26 00:04:12 -0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ClienteList from './components/ClienteList';
+import VendaList from './components/VendaList';
 
 function App() {
     return (
         <div className="App">
+            <h1>Controle de Vendas da Loja KX</h1>
             <ClienteList />
+            <VendaList />
         </div>
     );
 }
